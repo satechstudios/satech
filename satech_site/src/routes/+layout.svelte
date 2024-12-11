2976a00a9072c8840d90819d03d6faedb82e3220
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-11 17:16:19 +0530

    footer done

--- satech_site/src/routes/+layout.svelte
+++ satech_site/src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
     import "../app.css";
     import Navbar from "../components/navbar.svelte";
+    import Footer from "../components/footer.svelte";
 </script>
 
 <Navbar />
 <slot>
 </slot>
+<Footer />
