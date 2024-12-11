c6fa46a38b9e9c5a7cb700ff0ec9e88093a2a789
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-11 07:00:07 +0530

    Hero section

---
+++ satech_site/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css";
+    import Navbar from "../components/navbar.svelte";
+</script>
+
+<Navbar />
+<slot>
+</slot>
--- satech_site/src/routes/+page.svelte
+++ satech_site/src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import "./styles.css"
+</script>
+
+<div id="top-section" class="text-white relative">
+    <div class="absolute inset-0 bg-black opacity-85 z-0"></div> <!-- Background with opacity -->
+    <div class="relative z-10 h-full w-full flex flex-col justify-center">
+      <p class="md:text-6xl text-3xl font-bold uppercase self-center w-3/4 text-center my-5 py-5">
+        From Vision to Reality, Beyond Imagination
+      </p>
+      <p class="md:text-xl text-md self-center w-3/6 text-center">
+        We the talented and passionate team will work hard to bring your wildest dreams into reality with the help of AR/VR
+      </p>
+      <button
+        type="button"
+        class="text-center w-52 rounded-2xl h-14 relative text-black text-xl font-semibold group self-center my-5 z-10"
+      >
+        <div
+          class="bg-blue-900 rounded-xl h-12 w-1/4 flex items-center justify-end absolute left-1 top-[4px] group-hover:w-[240px] duration-500"
+        >
+          <svg
+            viewBox="0 0 256 256"
+            fill="#ffffff"
+            height="32"
+            width="32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"
+            ></path>
+          </svg>
+        </div>
+        <p class="translate-x-2 z-10 text-white h-12 rounded-xl bg-blue-900 p-2">
+          Order our Service
+        </p>
+      </button>
+    </div>
+  </div>
+  
