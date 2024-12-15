028121927b00c9e3e556f5ec777122192857e9f1
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-15 19:19:57 +0530

    contact form sheets integration

--- satech_site/src/routes/contact/+page.svelte
+++ satech_site/src/routes/contact/+page.svelte
@@ -1,3 +1,27 @@
+<script>
+  import { onMount } from 'svelte';
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz4xFrG1D8LDYBBJ8bkD9c7V2o2s11lcLgh1ZfsCNxIOBEN-nE7Os_IMlrFTvVyv-rq2w/exec';
+
+  let form;
+
+  onMount(() => {
+    form.addEventListener('submit', handleSubmit);
+    return () => {
+      form.removeEventListener('submit', handleSubmit);
+    };
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => alert("Thank you! Form is submitted"))
+      .then(() => { window.location.reload(); })
+      .catch(error => console.error('Error!', error.message));
+  }
+</script>
+
 <div class="flex flex-col items-center md:p-5 pt-5">
   <h2 class="text-4xl font-bold text-center">Follow Us</h2>
   <div class="flex flex-col md:w-max-xl justify-center w-full">
@@ -38,24 +62,23 @@
   </div>
 </div>
 
-
 <div class="flex md:p-5 pt-5 justify-center">
-  <form class="lg:w-1/2 w-[400px] p-3">
+  <form bind:this={form} class="lg:w-1/2 w-[400px] p-3" method="post" action="" name="contact-form">
     <div class="mb-5">
       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-      <input type="text" id="name" placeholder="John Doe" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <input name="name" type="text" id="name" placeholder="John Doe" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     </div>
     <div class="mb-5">
       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-      <input type="email" id="email" placeholder="john.doe@email.com" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <input name="email" type="email" id="email" placeholder="john.doe@email.com" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     </div>
     <div class="mb-5">
       <label for="phnno" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your phone number</label>
-      <input type="tel" id="phnno" placeholder="123-456-7890" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <input name="phnno" type="tel" id="phnno" placeholder="123-456-7890" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
     </div>
     <div class="mb-5">
       <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-      <textarea id="message" placeholder="Enter your message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+      <textarea name="message" id="message" placeholder="Enter your message" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
     </div>
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
   </form>
