ff6a2c7c9360a40a95e9e00cb9f1a5e92a3a480d
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-16 17:20:16 +0530

    added regex

--- satech_site/src/routes/contact/+page.svelte
+++ satech_site/src/routes/contact/+page.svelte
@@ -75,21 +75,28 @@
     <input type="hidden" name="access_key" value="76803e84-ecab-4c6b-8237-0d7a53f5e345">
     <div class="mb-5">
       <label for="name" class="block mb-2 text-sm font-medium text-white">Your name</label>
-      <input name="name" type="text" id="name" placeholder="John Doe" required class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg">
+      <input name="name" type="text" id="name" placeholder="John Doe" required pattern="[A-Za-z\s]+" title="Name should only contain letters and spaces." class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg">
     </div>
     <div class="mb-5">
       <label for="email" class="block mb-2 text-sm font-medium text-white">Your email</label>
-      <input name="email" type="email" id="email" placeholder="john.doe@email.com" required class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg">
+      <input 
+      name="email" 
+      type="email" 
+      id="email" 
+      placeholder="john.doe@email.com" 
+      required 
+      title="Please enter a valid email address." 
+      class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg">    
     </div>
     <div class="mb-5">
       <label for="phnno" class="block mb-2 text-sm font-medium text-white">Your phone number</label>
-      <input name="phnno" type="tel" id="phnno" placeholder="123-456-7890" required class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg">
+      <input name="phnno" type="text" inputmode="numeric" id="phnno" placeholder="123-456-7890" required pattern="[0-9]*" title="Phone number should be in the format: 123-456-7890." class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg">
     </div>
     <div class="mb-5">
       <label for="message" class="block mb-2 text-sm font-medium text-white">Your message</label>
       <textarea name="message" id="message" placeholder="Enter your message" required class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg"></textarea>
     </div>
     <button type="submit" class="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Submit</button>
-  </form>
+    </form>
 </div>
 <div>{status}</div>
