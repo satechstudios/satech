b52ed55dc21fe1ce39f2c70043956122b4fb9a88
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-16 12:33:12 +0530

    light theme corrected

--- satech_site/src/lib/components/navbar.svelte
+++ satech_site/src/lib/components/navbar.svelte
@@ -64,7 +64,7 @@
   };
 </script>
 
-<nav class="bg-white border-gray-200 dark:bg-gray-900 sticky top-0 z-50">
+<nav class= "border-gray-200 bg-gray-900 sticky top-0 z-50">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <!-- Logo -->
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -99,14 +99,14 @@
 
     <!-- Navigation Links -->
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700 text-white">
         {#each navItems as item}
-          <li class="relative">
+          <li class="relative text-white">
             {#if item.dropdown}
               <!-- Dropdown Toggle -->
               <button
                 on:click={() => toggleDropdown(item.name)}
-                class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                class="flex items-center justify-between w-full py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto text-white md:dark:hover:text-blue-500 focus:text-white border-gray-700"
               >
                 {item.name}
                 <svg
@@ -136,7 +136,7 @@
                       <li>
                         <a
                           href={subItem.href}
-                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          class="block px-4 py-2 hover:bg-gray-600 text-white"
                         >
                           {subItem.name}
                         </a>
@@ -150,7 +150,7 @@
               <!-- Normal Link -->
               <a
                 href={item.href}
-                class="block py-2 px-3 rounded md:p-0 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                class="block py-2 px-3 rounded md:p-0 hover:bg-gray-700 md:border-0 text-white md:hover:text-blue-500 dark:hover:bg-gray-700 hover:text-white md:hover:bg-transparent"
               >
                 {item.name}
               </a>
