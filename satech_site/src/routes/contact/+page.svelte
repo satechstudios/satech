d1a719109552e527c01cde09a6dd0ba2082b17d9
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-16 11:26:30 +0530

    changes done

--- satech_site/src/routes/contact/+page.svelte
+++ satech_site/src/routes/contact/+page.svelte
@@ -1,11 +1,7 @@
 
 <script>
   import { onMount } from 'svelte';
-
-  // Access environment variables prefixed with VITE_
-  const scriptURL = import.meta.env.VITE_SCRIPT_URL;
-  const accessKey = import.meta.env.VITE_ACCESS_KEY;
-
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbw6901srxECceQjqcFRhMleJ8vQEf8vQodMKBfiB-M51gdQ6cllm-dLaQBXTLXIY17-_A/exec';
   let form;
   let status = "";
 
@@ -90,7 +86,7 @@
 
 <div class="flex md:p-5 pt-5 justify-center">
   <form bind:this={form} class="lg:w-1/2 w-[400px] p-3" method="post" action="" name="contact-form" on:submit|preventDefault={handleSubmit2}>
-    <input type="hidden" name="access_key" value={accessKey}>
+    <input type="hidden" name="access_key" value="76803e84-ecab-4c6b-8237-0d7a53f5e345">
     <div class="mb-5">
       <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
       <input name="name" type="text" id="name" placeholder="John Doe" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
