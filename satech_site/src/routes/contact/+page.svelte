3fa1f46a4791d5872ee96effac0d83f4edabed12
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-16 12:40:29 +0530

    corrected light theme

--- satech_site/src/routes/contact/+page.svelte
+++ satech_site/src/routes/contact/+page.svelte
@@ -88,22 +88,22 @@
   <form bind:this={form} class="lg:w-1/2 w-[400px] p-3" method="post" action="" name="contact-form" on:submit|preventDefault={handleSubmit2}>
     <input type="hidden" name="access_key" value="76803e84-ecab-4c6b-8237-0d7a53f5e345">
     <div class="mb-5">
-      <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-      <input name="name" type="text" id="name" placeholder="John Doe" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <label for="name" class="block mb-2 text-sm font-medium text-white">Your name</label>
+      <input name="name" type="text" id="name" placeholder="John Doe" required class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg">
     </div>
     <div class="mb-5">
-      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-      <input name="email" type="email" id="email" placeholder="john.doe@email.com" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <label for="email" class="block mb-2 text-sm font-medium text-white">Your email</label>
+      <input name="email" type="email" id="email" placeholder="john.doe@email.com" required class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg">
     </div>
     <div class="mb-5">
-      <label for="phnno" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your phone number</label>
-      <input name="phnno" type="tel" id="phnno" placeholder="123-456-7890" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+      <label for="phnno" class="block mb-2 text-sm font-medium text-white">Your phone number</label>
+      <input name="phnno" type="tel" id="phnno" placeholder="123-456-7890" required class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg">
     </div>
     <div class="mb-5">
-      <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-      <textarea name="message" id="message" placeholder="Enter your message" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+      <label for="message" class="block mb-2 text-sm font-medium text-white">Your message</label>
+      <textarea name="message" id="message" placeholder="Enter your message" required class="bg-gray-700 border border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 rounded-lg"></textarea>
     </div>
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    <button type="submit" class="text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Submit</button>
   </form>
 </div>
 <div>{status}</div>
