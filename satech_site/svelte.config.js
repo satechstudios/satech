c8e37d9db6c00638fe2736cfe13b7bd297eede19
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-17 08:24:25 +0530

    check 1

--- satech_site/svelte.config.js
+++ satech_site/svelte.config.js
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 export default {
 	kit: {
 		adapter: adapter({
-			// see below for options that can be set here
+			fallback: '404.html'
 		})
 	}
 };
