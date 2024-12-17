3e7c8bd6c69eb517159eafe9bef9803a998a56c9
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-17 08:57:29 +0530

    check 7

--- satech_site/svelte.config.js
+++ satech_site/svelte.config.js
@@ -2,7 +2,18 @@ import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	kit: {
-		adapter: adapter({
-		})
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ status, path, referrer }) => {
+				// Ignore all 404 errors during prerendering
+				if (status === 404) {
+					console.warn(`Ignoring 404 error for ${path} (linked from ${referrer || 'unknown'})`);
+					return;
+				}
+
+				// Throw for other types of errors
+				throw new Error(`Unhandled error at ${path}`);
+			}
+		}
 	}
 };
