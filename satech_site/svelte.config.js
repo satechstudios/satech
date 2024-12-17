a19b1e3424fa8e7d1774abd89b1f52652f30aef5
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-17 08:45:37 +0530

    check 6

--- satech_site/svelte.config.js
+++ satech_site/svelte.config.js
@@ -1,15 +1,8 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: '$lib/404.html',
-			precompress: false,
-			strict: true
 		})
 	}
 };
