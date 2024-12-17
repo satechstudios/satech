8451525031e2b1e0db0d82602bf42fba931814a1
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-17 20:09:29 +0530

    course section rework

--- satech_site/src/routes/courses/[id]/+page.svelte
+++ satech_site/src/routes/courses/[id]/+page.svelte
@@ -4,9 +4,10 @@
     let selectedCourse = data.props;
 </script>
 
-<div class="bg-blue-800 rounded-lg m-5 p-5 text-white flex flex-col md:flex-row gap-5 transition-all duration-[1000ms] ease-in-out transform animate-slide-in-top">
-    <img src={selectedCourse.image} alt={selectedCourse.title} class="rounded-lg w-full md:w-1/3">
-    <div class="flex flex-col justify-center">
+<div class="rounded-lg md:m-5 md:p-5 m-2 text-white flex flex-col justify-center md:w-full">
+    <img src={selectedCourse.image} alt={selectedCourse.title} class="rounded-lg self-center w-full md:w-1/3">
+    <div class="flex justify-center w-full">
+        <div class="flex flex-col m-5 md:w-3/4">
         <h2 class="text-4xl font-bold mb-3">{selectedCourse.title}</h2>
         <p class="text-lg mb-5">{selectedCourse.description}</p>
         <p class="text-lg font-medium">Course Outline</p>
@@ -15,5 +16,8 @@
                 <li>{topic}</li>
             {/each}
         </ul>
+
+        </div>
+        
     </div>
 </div>
