6f08593d8224b86332bf6ad5c7e9d7bed54a80ff
SATech Studios <satechstudios@gmail.com>
2026-08-06 22:00:02 +0530

    Comment out Teammem component import
    Comment out import for Teammem component.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import "./styles.css";
   import Carousal from "$lib/components/carousal.svelte";
-  import Teammem from "$lib/components/teammem.svelte";
+  //import Teammem from "$lib/components/teammem.svelte";
 
   let items = [
     {
